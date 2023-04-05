2d266c3c04232be22f0b428a527fe7cc715a6998
ishan-mukherjee02 <ishan.mukherjee02@gmail.com>
2023-04-05 17:08:34 -0400

    started a basic cmd ui
    Signed-off-by: ishan-mukherjee02 <ishan.mukherjee02@gmail.com>

---
+++ cmdgui.py
@@ -0,0 +1,21 @@
+def researchOutput():
+    print("Would you like to store, edit or delete a trial?")
+    line = input()
+    
+
+
+def patientOutput():
+    print("Please enter any search terms: ")
+    line = input()
+    # load model and pass input into model
+    # model returns list of relevant trials
+    trials = ["List", "of", "trials"]
+    print("Here are a list of current trials in that area:\n", trials)
+
+
+print("Researcher [R] or Patient [P]?")
+line = input()
+if line == "r" or line == "R":
+    researchOutput()
+elif line == "p" or line == "P":
+    patientOutput()
