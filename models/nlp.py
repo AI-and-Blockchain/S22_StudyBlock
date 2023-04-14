dd794e96d60a6b90c2f264305c02d3abacdc046a
shanthni <57296126+shanthni@users.noreply.github.com>
2023-04-13 20:10:28 -0400

    Added function to add trials

--- models/nlp.py
+++ models/nlp.py
@@ -14,46 +14,51 @@ import time
 
 class Model:
     def __init__(self, csv):
-        self.docs = pd.read_csv(csv)
-        self.title = self.docs['Study'].tolist()
-        self.text = self.docs['Detailed Description'].tolist()
+        self.csv_name = csv
+
+        docs = pd.read_csv(csv)
+        self.title = docs['Study'].tolist()
+        self.text = docs['Detailed Description'].tolist()
         self.text = [i.lower() for i in self.text]
+        self.docs = pd.DataFrame({"Study": self.title, "Detailed Description": self.text})
 
+    def train_model(self):
         nlp = spacy.load("en_core_sci_sm")
-        tok_text = []
 
+        tok_text = []
         for doc in nlp.pipe(self.text, disable=["tagger", "parser", "ner", "lemmatizer"]):
             tok = [t.text for t in doc if (t.is_ascii and not t.is_punct and not t.is_space)]
             tok_text.append(tok)
 
-        ft_model = FastText(
-            sg=1,  # use skip-gram: usually gives better results
-            vector_size=100,  # embedding dimension (default)
-            window=10,  # window size: 10 tokens before and 10 tokens after to get wider context
-            min_count=1,  # only consider tokens with at least n occurrences in the corpus
-            negative=15,  # negative subsampling: bigger than default to sample negative examples more
-            min_n=2,  # min character n-gram
-            max_n=5  # max character n-gram
-        )
+        bm25 = BM25Okapi(tok_text)
+        with open('bm25result', 'wb') as bm25result_file:
+            pickle.dump(bm25, bm25result_file)
 
-        ft_model.build_vocab(tok_text)
+    def add_trial(self, title, description):
+        self.title.append(title)
+        self.text.append(description.lower())
 
-        ft_model.train(
-            tok_text,
-            epochs=6,
-            total_examples=ft_model.corpus_count,
-            total_words=ft_model.corpus_total_words)
+        self.docs = pd.DataFrame({"Study": self.title, "Detailed Description": self.text})
 
-        ft_model.save('_fasttext.model')
+        self.docs.to_csv(self.csv_name)
 
-        self.bm25 = BM25Okapi(tok_text)
+        tok_text = []
+        nlp = spacy.load("en_core_sci_sm")
+        for doc in nlp.pipe(self.text, disable=["tagger", "parser", "ner", "lemmatizer"]):
+            tok = [t.text for t in doc if (t.is_ascii and not t.is_punct and not t.is_space)]
+            tok_text.append(tok)
 
+        bm25 = BM25Okapi(tok_text)
+        with open('bm25result', 'wb') as bm25result_file:
+            pickle.dump(bm25, bm25result_file)
 
     def search(self, query):
         tokenized_query = query.lower().split(" ")
 
-        results = self.bm25.get_top_n(tokenized_query, self.text, n=3)
-        for i in results:
-            print(self.title[self.text.index(i)])
-            print(i)
-            print('\n')
+        with open('bm25result', 'rb') as bm25result_file:
+            bm25result = pickle.load(bm25result_file)
+            results = bm25result.get_top_n(tokenized_query, self.text, n=3)
+            for i in results:
+                print(self.title[self.text.index(i)])
+                print(i)
+                print('\n')
