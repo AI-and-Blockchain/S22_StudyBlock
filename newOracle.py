75f17b84e67c22a3ecab4a0acc8dd91fb3e8649b
Radhika Khurana <mail.radhikakhurana@gmail.com>
2023-04-18 12:18:00 -0400

    Create newOracle.py

---
+++ newOracle.py
@@ -0,0 +1,90 @@
+from pyteal import *
+from messaging.ClinicalTrialMessaging import ClinicalTrialMessaging
+from algosdk.v2client import algod
+from algosdk import encoding
+from algosdk import transaction
+
+# Algod credentials
+purestake_key = "YOUR_ALGOD_API_KEY"
+endpoint_address = "https://testnet-algorand.api.purestake.io/ps2"
+headers = {"X-API-Key": purestake_key}
+client = algod.AlgodClient(purestake_key, endpoint_address, headers=headers)
+
+# Account credentials
+oracle_address = "ORACLE_ADDRESS"
+oracle_mnemonic = "ORACLE_MNEMONIC"
+oracle_private_key = encoding.mnemonic_to_private_key(oracle_mnemonic)
+researcher_address = "RESEARCHER_ADDRESS"
+researcher_price = 5000
+
+# Atomic contract credentials
+contract_address = "CONTRACT_ADDRESS"
+contract_private_key = "CONTRACT_PRIVATE_KEY"
+contract_receiver = "CONTRACT_RECEIVER"
+contract_amount = 10000
+contract_data = "CONTRACT_DATA"
+
+# Setup clinical trial messaging
+messaging = ClinicalTrialMessaging()
+
+# Define PyTeal program for the oracle
+program = Seq([
+    App.globalPut(Bytes("oracleLastBlock"), Int(0)),
+    While(Txn.type_enum() == Int(1), Seq([
+        App.globalPut(Bytes("oracleLastBlock"), Txn.last_valid()),
+        If(messaging.message_received(Txn.sender()), Seq([
+            # Prepare the transaction group for the atomic transfer
+            Txn.sender() == Addr(researcher_address),
+            Txn.amount() == Int(researcher_price),
+            App.localPut(Bytes("data"), Txn.application_args[0]),
+            App.optedIn(Txn.sender(), App.id()),
+            App.optedIn(Txn.receiver(), App.id()),
+
+            # Create the payment transaction
+            payment_txn = Txn.application_args[1],
+            Txn.application_args.length() == Int(2),
+            payment_txn.type_enum() == Int(4),
+            payment_txn.sender() == Addr(contract_receiver),
+            payment_txn.receiver() == Addr(Txn.sender()),
+            payment_txn.amount() == Int(contract_amount),
+            payment_txn.close_remainder_to() == Global.zero_address(),
+
+            # Create the application transaction
+            app_txn = Txn.application_args[0],
+            app_txn.type_enum() == Int(5),
+            app_txn.on_completion() == OnComplete.NoOp,
+            app_txn.accounts[0] == Addr(researcher_address),
+            app_txn.accounts[1] == Addr(contract_receiver),
+            app_txn.foreign_apps.length() == Int(0),
+            app_txn.foreign_assets.length() == Int(0),
+            app_txn.app_args[0] == Bytes(contract_data),
+            app_txn.app_args[1] == payment_txn.txid(),
+
+            # Sign and submit the transaction group
+            payment_txn_bytes = payment_txn.get_obj_for_encoding(),
+            app_txn_bytes = app_txn.get_obj_for_encoding(),
+            txns = [
+                transaction.Transaction.payment(payment_txn_bytes),
+                transaction.Transaction.app_call(app_txn_bytes)
+            ],
+            signed_txns = [
+                payment_txn.sign(contract_private_key),
+                app_txn.sign(oracle_private_key)
+            ],
+            txid = client.send_transactions(signed_txns)
+        ]))
+    ]))
+])
+
+# Compile and save the PyTeal program
+with open("oracle.teal", "w") as f:
+    compiled = compileTeal(program, Mode.Application)
+    f.write(compiled)
+
+# Get the latest confirmed block number
+last_block = client.status()["lastRound"]
+
+# Run the oracle
+while True:
+    try:
+        result = client.pending_transactions
